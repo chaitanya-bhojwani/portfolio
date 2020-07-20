fd2901abd9912890fdc46a24fd2c331a361aee11
Chaitanya Bhojwani <chaitanya.b1@olacabs.com>
2020-07-20 23:00:46 +0530

    Updates

---
+++ precache-manifest.ee2e54cf74df2dd8bed582af47feea2f.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d99600f91dadcbef96b7c49d9ca2e76a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "14fd11a561b689bb15b3",
+    "url": "/portfolio/static/css/3.cffc9e54.chunk.css"
+  },
+  {
+    "revision": "d2e24fdb8e1d0cc42642",
+    "url": "/portfolio/static/css/4.d2b3893c.chunk.css"
+  },
+  {
+    "revision": "725a54f45d6a37438796",
+    "url": "/portfolio/static/css/main.a939b405.chunk.css"
+  },
+  {
+    "revision": "c47b51f81aea7495eb01",
+    "url": "/portfolio/static/js/2.eea4f165.chunk.js"
+  },
+  {
+    "revision": "14fd11a561b689bb15b3",
+    "url": "/portfolio/static/js/3.0d836c63.chunk.js"
+  },
+  {
+    "revision": "d2e24fdb8e1d0cc42642",
+    "url": "/portfolio/static/js/4.0e473a6d.chunk.js"
+  },
+  {
+    "revision": "725a54f45d6a37438796",
+    "url": "/portfolio/static/js/main.502f24d8.chunk.js"
+  },
+  {
+    "revision": "2b11b8df995f64619888",
+    "url": "/portfolio/static/js/runtime-main.19e8992d.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/portfolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/portfolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "90ebbff1515c4bc6fbe2dc840d312d09",
+    "url": "/portfolio/static/media/arcs.90ebbff1.png"
+  },
+  {
+    "revision": "68e841388612be0c5143e2932fb77d4f",
+    "url": "/portfolio/static/media/contactMail.68e84138.webp"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/portfolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "a7b9bc3a192b49255f3856a180957d98",
+    "url": "/portfolio/static/media/ieee-cs-logo.a7b9bc3a.png"
+  },
+  {
+    "revision": "895c91a3cfd132a3a52bd5364ab6493d",
+    "url": "/portfolio/static/media/kafka-white.895c91a3.png"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/portfolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "cc080e0300d0bd16dfb19c77b2386585",
+    "url": "/portfolio/static/media/messageClassifier.cc080e03.webp"
+  },
+  {
+    "revision": "ff371bee2822155539a4e602198b327e",
+    "url": "/portfolio/static/media/mqtt-white.ff371bee.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/portfolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "0288c5ec7a87ea73f8d14b12fc4bc94e",
+    "url": "/portfolio/static/media/talksCardBack.0288c5ec.svg"
+  },
+  {
+    "revision": "193d6d721b5dc189be79b2c467155d73",
+    "url": "/portfolio/static/media/vertx-white.193d6d72.png"
+  }
+]);
